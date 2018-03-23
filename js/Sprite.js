27c2a026ef84bce73bdf6afe3542cab22a3fabe4
Kyle Lowery <Loweryk1@ferris.edu>
2018-03-23 13:50:18 -0400

    Sprite Test
    The sprites are now somewhat working within the program. Need more sprites/logic to build further.

--- js/Sprite.js
+++ js/Sprite.js
@@ -3,14 +3,15 @@ function Sprite(url){
     this.img = new Image();
     this.img.src = url;
     this.img.addEventListener("load", ()=>{
-        this.anchor.x = this.img.width/2;
-        this.anchor.y = this.img.height/2;
+        
+        this.anchor.x = 0;
+        this.anchor.y = (this.img.height > 10)? 10 : 0;
     });
     
     this.x = 0;
     this.y = 0;
     this.angle = 0;
-    this.scale = 1;
+    this.scale = 2.5;
     this.anchor = {x:0, y:0};
     
     this.draw = function(gfx){
--- js/Tile.js
+++ js/Tile.js
@@ -2,28 +2,44 @@
 function Tile(){
     this.x = 0;
     this.y = 0;
+    this.gridX = 0;
+    this.gridY = 0;
     this.w = 25;
     this.h = 25;
     this.color = "#000";
     this.win = null;
+    this.sprite = null;
     
     this.tileType = "tile"; //tile(floor), door code(insert the name of the room this door leads to), or wall for now
     
     this.init = function(x, y, color){
+        switch(this.tileType){
+            case "wall" :
+                if((x == 0 && y == 0) || (x == 90 && y == 0) || (x == 0 && y == 90) || (x == 90 && y == 90)){
+                   this.sprite = new Sprite("../imgs/floor_layout/tile-wall-corner.png");
+                    break; 
+                }
+                this.sprite = new Sprite("../imgs/floor_layout/tile-wall.png");
+                break;
+            case "    " :
+                this.sprite = new Sprite("../imgs/floor_layout/tile-floor.png");
+                break;
+            default :
+                this.sprite = new Sprite("../imgs/player/player-test.png");
+                break;
+        } 
+        console.log(this.sprite);
         this.x = x;
         this.y = y;
+        this.sprite.x = this.x;
+        this.sprite.y = this.y;
         this.color = color;
     };
     this.update = function(dt){
         
     };
     this.draw = function(gfx){
-        gfx.fillStyle = this.color;
-        gfx.translate(this.x, this.y);
-        gfx.strokeStyle = '#000';
-        gfx.fillRect(0, 0, this.w, this.h);
-        gfx.strokeRect(0, 0, this.w, this.h);
-        gfx.resetTransform();
+        this.sprite.draw(gfx);
         
     };
     this.replicate = function(other){
