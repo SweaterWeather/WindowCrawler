03451d4a9b22379fff917e25a66b7031fe8d1a4f
Kyle Lowery <Loweryk1@ferris.edu>
2018-03-23 15:20:33 -0400

    Dungeon Layout sprites implemented
    Added a bunch of sprites to the game, and those are now able to build out the dungeon layouts visually.

--- js/Tile.js
+++ js/Tile.js
@@ -13,27 +13,81 @@ function Tile(){
     this.tileType = "tile"; //tile(floor), door code(insert the name of the room this door leads to), or wall for now
     
     this.init = function(x, y, color){
+        this.x = x;
+        this.y = y;
+        
+        this.gridX = this.x / this.w;
+        this.gridY = this.y / this.h;
+        
         switch(this.tileType){
             case "wall" :
-                if((x == 0 && y == 0) || (x == 90 && y == 0) || (x == 0 && y == 90) || (x == 90 && y == 90)){
-                   this.sprite = new Sprite("../imgs/floor_layout/tile-wall-corner.png");
+                //if wall corners:
+                if((this.gridX == 0 && this.gridY == 0)){
+                   this.sprite = new Sprite("../imgs/floor_layout/tile-wall-corner-topLeft.png");
                     break; 
+                } else if((this.gridX == 9 && this.gridY == 0)){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-corner-topRight.png");
+                    break;   
+                } else if((this.gridX == 0 && this.gridY == 9)){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-corner-bottomLeft.png");
+                    break;
+                } else if((this.gridX == 9 && this.gridY == 9)) {
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-corner-bottomRight.png");
+                    break;
+                //if solid wall:
+                } else {
+                    if(this.gridX == 0) {
+                        this.sprite = new Sprite("../imgs/floor_layout/tile-wall-left.png");
+                        break;
+                    } else if(this.gridX == 9) {
+                        this.sprite = new Sprite("../imgs/floor_layout/tile-wall-right.png");
+                        break;
+                    } else if(this.gridY == 0) {
+                        this.sprite = new Sprite("../imgs/floor_layout/tile-wall-top.png");
+                        break;
+                    } else if(this.gridY == 9) {
+                        this.sprite = new Sprite("../imgs/floor_layout/tile-wall-bottom.png");
+                        break;
+                    }
                 }
-                this.sprite = new Sprite("../imgs/floor_layout/tile-wall.png");
-                break;
             case "    " :
                 this.sprite = new Sprite("../imgs/floor_layout/tile-floor.png");
                 break;
             default :
-                this.sprite = new Sprite("../imgs/player/player-test.png");
-                break;
+                //if entryway
+                if(this.gridX == 0 && this.gridY == 4){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-entry-leftUp.png");
+                    break;
+                } else if(this.gridX == 0 && this.gridY == 5){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-entry-leftDown.png");
+                    break;
+                } else if(this.gridX == 9 && this.gridY == 4){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-entry-rightUp.png");
+                    break;
+                } else if(this.gridX == 9 && this.gridY == 5){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-entry-rightDown.png");
+                    break;
+                } else if(this.gridX == 4 && this.gridY == 0){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-entry-upLeft.png");
+                    break;
+                } else if(this.gridX == 5 && this.gridY == 0){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-entry-upRight.png");
+                    break;
+                } else if(this.gridX == 4 && this.gridY == 9){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-entry-downLeft.png");
+                    break;
+                } else if(this.gridX == 5 && this.gridY == 9){
+                    this.sprite = new Sprite("../imgs/floor_layout/tile-wall-entry-downRight.png");
+                    break;
+                } else {
+                    this.sprite = new Sprite("../imgs/player/player-test.png");
+                }
         } 
-        console.log(this.sprite);
-        this.x = x;
-        this.y = y;
+        
         this.sprite.x = this.x;
         this.sprite.y = this.y;
         this.color = color;
+        
     };
     this.update = function(dt){
         
